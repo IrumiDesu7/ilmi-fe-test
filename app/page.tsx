e12ec6c3203f493f3f4fb11dd454aa6f6a0164e3
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2025-02-04 21:45:33 +0800

    feat: add Hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/sections/hero";
+
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl px-4 py-6 font-[family-name:var(--font-geist-sans)] sm:px-6 lg:px-8 lg:py-16">
-      <div className="space-y-8">This is main section</div>
+      <Hero />
     </main>
   );
 }
