26fca0ea789c67a338396f0321739527542bf7d1
Ilmi <83828336+IrumiDesu7@users.noreply.github.com>
2025-02-05 15:13:57 +0800

    feat: enhance Hero section with motion animations and call-to-action

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Features from "@/components/sections/features";
 import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl px-4 py-6 font-[family-name:var(--font-geist-sans)] sm:px-6 lg:px-8 lg:py-16">
       <Hero />
+      <Features />
     </main>
   );
 }
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,21 +1,54 @@
 "use client";
 
 import { motion } from "motion/react";
+import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <motion.h1
-      initial={{ opacity: 0.5, y: 100 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{
-        delay: 0.3,
-        duration: 0.8,
-        ease: "easeInOut",
-      }}
-      className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text py-4 text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+    <div
+      id="hero"
+      className="flex min-h-[60vh] flex-col items-center justify-center gap-4"
     >
-      Build lamps <br /> the right way
-    </motion.h1>
+      <motion.h1
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ margin: "-100px" }}
+        transition={{
+          duration: 0.6,
+          ease: "easeOut",
+        }}
+        className="mt-8 text-balance bg-gradient-to-br from-slate-300 to-slate-500 bg-clip-text py-4 text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      >
+        Your Canvas for Creative & Financial Innovation
+      </motion.h1>
+      <motion.p
+        className="text-slate-700"
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ margin: "-100px" }}
+        transition={{
+          duration: 0.6,
+          delay: 0.2,
+          ease: "easeOut",
+        }}
+      >
+        Complete <span className="font-semibold">ownership</span> and{" "}
+        <span className="font-semibold">control</span> of your digital assets
+        through blockchain technology
+      </motion.p>
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ margin: "-100px" }}
+        transition={{
+          duration: 0.6,
+          delay: 0.4,
+          ease: "easeOut",
+        }}
+      >
+        <Button>Get Started</Button>
+      </motion.div>
+    </div>
   );
 };
 
